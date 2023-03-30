9be9cebe332c342234dc353ff2cdd7a83f31c953
Diez <62673882+zeidfazazi@users.noreply.github.com>
2023-03-30 12:23:08 +0200

    Add code to start playing with the API
    Co-authored-by: shuhan-duan <shuhan-duan@users.noreply.github.com>

--- choixdememe-server/main.go
+++ choixdememe-server/main.go
@@ -1,19 +1,54 @@
-package choixdememe
+package main
 
 import (
-	"io"
+	/* "gorm.io/gorm" */
+	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/peterhellberg/giphy"
 )
 
+type RandomResponse struct {
+	ID  string `json:"id"`
+	URL string `json:"url"`
+}
+
+type HelloResponse struct {
+	Message string `json:"message"`
+}
+
 func main() {
-	// Hello world, the web server
+	g := giphy.DefaultClient
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+	key, _ := os.LookupEnv("GIPHY_API_KEY")
+	g.APIKey = key
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+		w.Header().Set("Content-Type", "application/json")
+		res := HelloResponse{Message: "Hello World !"}
+		json.NewEncoder(w).Encode(res)
+	}
+
+	randomHandler := func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		res, err := g.Trending()
+		if err != nil {
+			res := HelloResponse{Message: "Erreur"}
+			json.NewEncoder(w).Encode(res)
+		} else {
+			gif := res.Data[rand.Intn(len(res.Data))]
+			res := RandomResponse{ID: gif.Caption, URL: gif.BitlyGifURL}
+			json.NewEncoder(w).Encode(res)
+		}
 	}
 
 	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listing for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	http.HandleFunc("/random", randomHandler)
+	http.ListenAndServe(":8000", nil)
 }

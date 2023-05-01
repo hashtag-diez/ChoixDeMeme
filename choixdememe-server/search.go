3d1ec4b434d1c097cc4496da8ec22ab684192686
Diez <62673882+zeidfazazi@users.noreply.github.com>
2023-05-01 17:48:53 +0200

    API route for GIF searching

---
+++ choixdememe-server/search.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/peterhellberg/giphy"
+)
+
+type GIF struct {
+	Caption string `json:"caption"`
+	Link    string `json:"link"`
+}
+
+func searchHandler(g *giphy.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		_, err := getUserIDFromRequest(req, db)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		if req.Method == "GET" {
+			// Parse the query parameters
+			keyword := req.URL.Query().Get("keyword")
+			if keyword == "" {
+				http.Error(w, "Unsupported HTTP request query", http.StatusBadRequest)
+				return
+			}
+
+			// Retrieve the duels
+			var duels []GIF
+
+			args := make([]string, 1)
+			args = append(args, keyword)
+			result, err := g.Search(args)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			for i := 0; i < len(result.Data); i++ {
+				curr := result.Data[i]
+				c := curr.Caption
+				if c == "" {
+					c = curr.Username + " GIF"
+				}
+				gif := GIF{Caption: c, Link: curr.MediaURL()}
+				duels = append(duels, gif)
+			}
+			// Return the duels as a response
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(duels)
+
+		} else {
+			http.Error(w, "Unsupported HTTP method", http.StatusMethodNotAllowed)
+			return
+		}
+	}
+}
